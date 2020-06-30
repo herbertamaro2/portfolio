0327a1232ceeb29a55d796f50f2c8a38bdec8a22
herbertsantana <herbert.webdesigner@gmail.com>
2020-06-30 15:57:29 +0100

    Novas alterações

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import Header from '../../pages/template/header';
 import Footer from '../../pages/template/footer';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './style.css';
 
 function Home() {
   return (
     <div className="App">
     <Header />
-    <section className="content">
+    <section className="content py-5 px-5">
     <h1>HERBERT AMARO SANTANA</h1>
         <p>
           Web Developer, I work in the creation and development environment. 24, native of Itaquaquecetuba / SP, has experience in frontend and web designer, as well as working in the digital environment as interface designer for websites, branding, mobile applications and social networks. Creative and active, I am always looking for new knowledge to attend to the rapid changes of the market, the internet advance and its social networks, with the purpose of increasing sales of my clients and strengthening their brand in the digital environment.
--- src/pages/template/header.js
+++ src/pages/template/header.js
@@ -1,13 +1,44 @@
 import React from 'react';
 import Logo from '../../assets/img/logo.gif';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Nav from 'react-bootstrap/Nav';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Navbar from 'react-bootstrap/Navbar';
 import './style.css';
 
 function Header() {
  return (
   <div>
   <header>
-  <img src={Logo} />
+  <div className="row m-0 py-4">
+	  <div className="logo col-3 col-xs-8">
+	  	
+	  </div>
+	  
+  </div>
   </header>
+
+  <Navbar bg="light" expand="lg">
+  <Navbar.Brand href="#home"><img src={Logo} /></Navbar.Brand>
+  <Navbar.Toggle aria-controls="basic-navbar-nav" />
+  <div className="contacte-us text-center col-6">
+	  Contact us and ask for a quote<br />
+	 +353 83 381 8131
+	  </div>
+  <Navbar.Collapse id="basic-navbar-nav">
+    <Nav className="mr-auto">
+      <Nav.Link href="#home">Home</Nav.Link>
+      <Nav.Link href="#link">About Me</Nav.Link>
+      <NavDropdown title="Services" id="basic-nav-dropdown">
+        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+      </NavDropdown>
+      <Nav.Link href="#link">Portfolio</Nav.Link>
+      <Nav.Link href="#link">Contact</Nav.Link>
+    </Nav>
+  </Navbar.Collapse>
+</Navbar>
   </div>
   );
 
