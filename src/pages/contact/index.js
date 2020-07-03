ff53834c81a7f5ee73125b347f3e1c4bb2219543
herbertsantana <herbert.webdesigner@gmail.com>
2020-07-03 22:11:48 +0100

    Contact

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from '../../pages/template/header';
 import Footer from '../../pages/template/footer';
+import ContactImg from '../../assets/img/contact.svg';
 import './style.css';
 
 function Contact() {
@@ -9,22 +10,30 @@ function Contact() {
     <Header />
     <section className="content content-contact py-5 px-5">
     <h1>Contact</h1>
-    <div class="content-page">
-		<form id="contact-form"  method="POST">
-		    <div className="form-group">
-		        <label htmlFor="name">Name</label>
-		        <input type="text" className="form-control" />
-		    </div>
-		    <div className="form-group">
-		        <label htmlFor="exampleInputEmail1">Email address</label>
-		        <input type="email" className="form-control" aria-describedby="emailHelp" />
-		    </div>
-		    <div className="form-group">
-		        <label htmlFor="message">Message</label>
-		        <textarea className="form-control" rows="5"></textarea>
-		    </div>
-		    <button type="submit" className="btn btn-primary">Submit</button>
-		</form>
+    <div className="content-page">
+    	<div className="row m-0">
+    	<div className="col-6 col-xs-12">
+			<form id="contact-form"  method="POST">
+				<div className="form-group">
+			        <label htmlFor="name">Name</label>
+			        <input type="text" className="form-control" />
+			    </div>
+			    <div className="form-group">
+			        <label htmlFor="exampleInputEmail1">Email address</label>
+			        <input type="email" className="form-control" aria-describedby="emailHelp" />
+			    </div>
+			    <div className="form-group">
+			        <label htmlFor="message">Message</label>
+			        <textarea className="form-control" rows="5"></textarea>
+			    </div>
+			    <button type="submit" className="btn btn-primary">Submit</button>
+			</form>
+		</div>
+		<div className="col-6 col-xs-12">
+			<img src={ContactImg} />
+		</div>
+		</div>
+
     </div>
     </section>
     <Footer />  
