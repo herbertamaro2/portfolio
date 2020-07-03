c8fe91b085b0be8844285ee0e316a64d17b1e1e1
herbertsantana <herbert.webdesigner@gmail.com>
2020-07-03 21:54:20 +0100

    Alterações Contact

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -9,6 +9,23 @@ function Contact() {
     <Header />
     <section className="content content-contact py-5 px-5">
     <h1>Contact</h1>
+    <div class="content-page">
+		<form id="contact-form"  method="POST">
+		    <div className="form-group">
+		        <label htmlFor="name">Name</label>
+		        <input type="text" className="form-control" />
+		    </div>
+		    <div className="form-group">
+		        <label htmlFor="exampleInputEmail1">Email address</label>
+		        <input type="email" className="form-control" aria-describedby="emailHelp" />
+		    </div>
+		    <div className="form-group">
+		        <label htmlFor="message">Message</label>
+		        <textarea className="form-control" rows="5"></textarea>
+		    </div>
+		    <button type="submit" className="btn btn-primary">Submit</button>
+		</form>
+    </div>
     </section>
     <Footer />  
     </div>
