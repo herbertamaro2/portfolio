c73062717a698486c6f2d2a2e20f4d64b9aa8237
herbertsantana <herbert.webdesigner@gmail.com>
2020-07-08 21:05:51 +0100

    Alterações

--- src/pages/portfolio/category/identify.js
+++ src/pages/portfolio/category/identify.js
@@ -1,11 +1,37 @@
-import React from 'react';
+import React from "react";
+import Carousel, { Modal, ModalGateway } from 'react-images';		
 
-function Identify() {
-  return (
-    <div>
-    	Identidade Visual
-    </div>
-  );
+
+const images =
+[{
+        src: "https://c2.staticflickr.com/9/8817/28973449265_07e3aa5d2e_b.jpg"
+},
+{
+        src: "https://c2.staticflickr.com/9/8356/28897120681_3b2c0f43e0_b.jpg"
+},
+
+{
+        src: "https://c4.staticflickr.com/9/8887/28897124891_98c4fdd82b_b.jpg"
+}];
+
+class Identify extends React.Component {
+  state = { modalIsOpen: true }
+  toggleModal = () => {
+    this.setState(state => ({ modalIsOpen: !state.modalIsOpen }));
+  }
+  render() {
+    const { modalIsOpen } = this.state;
+
+    return (
+      <ModalGateway>
+        {modalIsOpen ? (
+          <Modal onClose={this.toggleModal}>
+            <Carousel views={images} />
+          </Modal>
+        ) : null}
+      </ModalGateway>
+    );
+  }
 }
 
 export default Identify;
--- src/pages/portfolio/category/websites.js
+++ src/pages/portfolio/category/websites.js
@@ -11,78 +11,77 @@ function Websites() {
 
     	<div className="section-page">
     		<ul className="row m-0 p-0">
-    		<li className="list-portfolio col-12 text-left d-flex">
-    			<div className="block-img">	
-    				<img src="https://herbertamaro.com/wp-content/uploads/2018/02/viagenslovers.jpg" />
-    			</div>
-    			<div class="block-title">
-    			<h2>SuiteCred</h2>
-    			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vitae metus vel lorem tincidunt ornare vel non ex.</p>
-    			</div>
-    		</li>
-    		<li className="list-portfolio col-12 text-left d-flex">
-    			<div className="block-img">	
-    				<img src="https://herbertamaro.com/wp-content/uploads/2018/02/viagenslovers.jpg" />
-    			</div>
-    			<div class="block-title">
-    			<h2>SuiteCred</h2>
-    			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vitae metus vel lorem tincidunt ornare vel non ex.</p>
-    			</div>
-    		</li>
-    		<li className="list-portfolio col-12 text-left d-flex">
-    			<div className="block-img">	
-    				<img src="https://herbertamaro.com/wp-content/uploads/2018/02/viagenslovers.jpg" />
-    			</div>
-    			<div class="block-title">
-    			<h2>SuiteCred</h2>
-    			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vitae metus vel lorem tincidunt ornare vel non ex.</p>
-    			</div>
-    		</li>
-    		<li className="list-portfolio col-12 text-left d-flex">
-    			<div className="block-img">	
-    				<img src="https://herbertamaro.com/wp-content/uploads/2018/02/viagenslovers.jpg" />
-    			</div>
-    			<div class="block-title">
-    			<h2>SuiteCred</h2>
-    			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vitae metus vel lorem tincidunt ornare vel non ex.</p>
-    			</div>
-    		</li>
-    		<li className="list-portfolio col-12 text-left d-flex">
-    			<div className="block-img">	
-    				<img src="https://herbertamaro.com/wp-content/uploads/2018/02/viagenslovers.jpg" />
-    			</div>
-    			<div class="block-title">
-    			<h2>SuiteCred</h2>
-    			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vitae metus vel lorem tincidunt ornare vel non ex.</p>
-    			</div>
-    		</li>
-    		<li className="list-portfolio col-12 text-left d-flex">
-    			<div className="block-img">	
-    				<img src="https://herbertamaro.com/wp-content/uploads/2018/02/viagenslovers.jpg" />
-    			</div>
-    			<div class="block-title">
-    			<h2>SuiteCred</h2>
-    			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vitae metus vel lorem tincidunt ornare vel non ex.</p>
-    			</div>
-    		</li>
-    		<li className="list-portfolio col-12 text-left d-flex">
-    			<div className="block-img">	
-    				<img src="https://herbertamaro.com/wp-content/uploads/2018/02/viagenslovers.jpg" />
-    			</div>
-    			<div class="block-title">
-    			<h2>SuiteCred</h2>
-    			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vitae metus vel lorem tincidunt ornare vel non ex.</p>
-    			</div>
-    		</li>
-    		<li className="list-portfolio col-12 text-left d-flex">
-    			<div className="block-img">	
-    				<img src="https://herbertamaro.com/wp-content/uploads/2018/02/viagenslovers.jpg" />
-    			</div>
-    			<div class="block-title">
-    			<h2>SuiteCred</h2>
-    			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vitae metus vel lorem tincidunt ornare vel non ex.</p>
-    			</div>
-    		</li>
+	    		<li className="list-portfolio col-12 text-left d-flex">
+	    			<div className="block-img">	
+	    				<img src="https://herbertamaro.com/wp-content/uploads/2018/02/viagenslovers.jpg" />
+	    			</div>
+	    			<div class="block-title">
+	    			<h2>Viagens Lovers - Rede Social</h2>
+	    			<p>Uma rede social, para mochileiros, trabalho completo.</p>
+					<ul>
+						<li><strong>Trabalho desenvolvido:</strong> Layout + Front + Gerenciável + App Mobile</li>
+						<li><strong>Tecnologia:</strong> HTML5, Javascript, CSS3, PHP</li>
+					</ul>
+	    			</div>
+	    		</li>
+	    		<li className="list-portfolio col-12 text-left d-flex">
+	    			<div className="block-img">	
+	    				<img src="https://herbertamaro.com/wp-content/uploads/2018/02/playerlink.jpg" />
+	    			</div>
+	    			<div class="block-title">
+	    			<h2>Playerlink - League of Legends</h2>
+	    			<p>Front desenvolvido para a Playerlink um site voltado ao mundo do e-Sports.</p>
+					<ul>
+						<li><strong>Trabalho desenvolvido:</strong> Layout + Front + Gerenciável</li>
+						<li><strong>Tecnologia:</strong> HTML5, Javascript, CSS3, PHP, Wordpress</li>
+					</ul>
+	    			</div>
+	    		</li>
+	    		<li className="list-portfolio col-12 text-left d-flex">
+	    			<div className="block-img">	
+	    				<img src="https://herbertamaro.com/wp-content/uploads/2018/02/viagenslovers.jpg" />
+	    			</div>
+	    			<div class="block-title">
+	    			<h2>Seu Programador - Site de Captação  de Clientes </h2>
+	    			<p>Uma rede social, para mochileiros, trabalho completo.</p>
+					<ul>
+						<li><strong>Trabalho desenvolvido:</strong> Layout + Front + Gerenciável + App Mobile</li>
+						<li><strong>Tecnologia:</strong> HTML5, Javascript, CSS3, PHP</li>
+					</ul>
+	    			</div>
+	    		</li>
+	    		<li className="list-portfolio col-12 text-left d-flex">
+	    			<div className="block-img">	
+	    				<img src="https://herbertamaro.com/wp-content/uploads/2017/08/letrasaqui.jpg" />
+	    			</div>
+	    			<div class="block-title">
+	    			<h2>Letras aqui - Letras de Músicas</h2>
+	    			<p>Serviços de músicas online, onde pode ver e ouvir músicas de diversos tipos e gêneros.</p>
+					<ul>
+						<li><strong>Trabalho desenvolvido:</strong> Logo + Layout + Front + Gerenciável</li>
+						<li><strong>Tecnologia:</strong> HTML5, Javascript, CSS3, PHP</li>
+
+					</ul>
+	    			</div>
+	    		</li>
+
+	    		<li className="list-portfolio col-12 text-left d-flex">
+	    			<div className="block-img">	
+	    				<img src="https://herbertamaro.com/wp-content/uploads/2017/08/agenciadeideias.jpg" />
+	    			</div>
+	    			<div class="block-title">
+	    			<h2>Agência de Idéias - Agência Web</h2>
+	    			<p>Site desenvolvido para agência digital, Agência de idéias.</p>
+					<ul>
+						<li><strong>Trabalho desenvolvido:</strong> Logo + Layout + Front + Gerenciável</li>
+						<li><strong>Tecnologia:</strong>Reactjs, HTML5, Javascript, CSS3, PHP</li>
+
+					</ul>
+	    			</div>
+	    		</li>
+
+
+
     		</ul>
 
 
