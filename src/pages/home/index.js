fe52df15a5a93fedf4104c0f2cb00df1b0e92a29
herbertsantana <herbert.webdesigner@gmail.com>
2020-07-13 18:26:47 +0100

    Alterações

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -11,8 +11,9 @@ function Home() {
     <Header />
     <section className="content content-about py-5 px-5">
     <h4>Frontend Developer</h4>
-    <h1>About HERBERT AMARO SANTANA</h1>
-        <p class="mx-5 px-3">
+     
+    <h1>HERBERT AMARO SANTANA</h1>
+        <p className="mx-5 px-5">
           Web Developer, I work in the creation and development environment. 24, native of Itaquaquecetuba / SP, has experience in frontend and web designer, as well as working in the digital environment as interface designer for websites, branding, mobile applications and social networks. Creative and active, I am always looking for new knowledge to attend to the rapid changes of the market, the internet advance and its social networks, with the purpose of increasing sales of my clients and strengthening their brand in the digital environment.
         </p>
         <ul className="techno-list list-inline">
@@ -29,6 +30,8 @@ function Home() {
         <li className="list-inline-item"><FaWordpress /></li>
         <li className="list-inline-item"><FaAdobe /></li>
         </ul>
+        
+        
         
     </section>
     <Footer />  
